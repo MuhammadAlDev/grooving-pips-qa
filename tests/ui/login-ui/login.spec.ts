0a7b8197d0c24d4a3659ed617377ad0edbc46f78
Muhammad Al <MuhammadAlDev@gmail.com>
2026-09-23 11:18:07 +0700

    edit import script

--- tests/ui/login-ui/login.spec.ts
+++ tests/ui/login-ui/login.spec.ts
@@ -1,36 +1,24 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../pages/login-page';
+import { LoginPage } from '../../pages/login-pages/LoginPage';
 import { testDataLogin } from '../../fixtures/test_data_login';
 
 for (const data of testDataLogin) {
   test(`Login - ${data.scenario}`, async ({ page }) => {
     const loginPage = new LoginPage(page);
 
-    // Open login page
     await loginPage.goto();
 
-    // Verify login page
-    await expect(
-      page.getByText('Sign in to your account to continue')
-    ).toBeVisible();
+    await loginPage.expectLoginPage();
 
-    // Perform login
     await loginPage.login(
       data.email,
       data.password
     );
 
-    // =========================
-    // VALID LOGIN
-    // =========================
     if (data.expectedType === 'success') {
-      // Sesuaikan dengan halaman setelah login berhasil
       await expect(page).not.toHaveURL(/login/);
     }
 
-    // =========================
-    // INVALID EMAIL / PASSWORD
-    // =========================
     if (data.expectedType === 'error') {
       await expect(
         page.getByText(data.expectedResult, {
@@ -39,9 +27,6 @@ for (const data of testDataLogin) {
       ).toBeVisible();
     }
 
-    // =========================
-    // EMPTY CREDENTIALS
-    // =========================
     if (data.expectedType === 'validation') {
       await loginPage.expectEmailInvalid();
 
